170f87e3763e974353a26e8a70f3aa36fb988776
Eddy Guo <eddyyanruguo@gmail.com>
2023-09-06 01:35:37 -0400

    update goals

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,12 @@ export default function Home() {
 }
 
 // TEST VALUES
-// 0xf73dbce07870ae4eca6c64fe287d42177875d529
-// 0x3057c07839f654f7fad4eb75d2dcbe093572cb11
+// default: 0xf73dbce07870ae4eca6c64fe287d42177875d529
+// contract creation: 0x9aa99c23f67c81701c772b106b4f83f6e858dd2e
 
 // MISC ISSUES
-// BigInt for dividing (show in eth instead of wei)
-  // CAN MULTIPLY min_gas BY BIGINT 10^18, but ideally other side (think, divide one side, might as well multiply other)
 // seperate from/to; have both (interactions), and have seperate (from a to b)
 
 // NEXT UP
-// txlist: date, transaction status(fail/success), contract creation, 
 // tokentx,tokennfttx,token1155tx: token transfer
-// HARD: events/event logs, contract specific calls/methods
+// HARD: events/event logs, contract specific calls/methods, date (implement calendar)
